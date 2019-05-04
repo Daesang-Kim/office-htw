c89778805a42ed1b0e816cb3282c03e276a39e27
Thor.Kim <kimdaesang@gmail.com>
2019-05-05 00:26:07 +0900

    [Style] Header 버튼 간격을 조정 (너무 붙어있었음)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,8 +7,8 @@ const Header = () => (
     <>
         <div>
           <h1>Welcome to HTW</h1>
-          <Link to="/WorkingTime">퇴능</Link>
-          <Link to="/ShuttleBus">셔틀</Link>
+          <Link to="/WorkingTime" style={{margin: "5px"}}>퇴능</Link>
+          <Link to="/ShuttleBus" style={{margin: "5px"}}>셔틀</Link>
           <hr />
         </div>
     </>
