de7ea9764996610db4a66c57b87507ea20110d0f
Thor.Kim <kimdaesang@gmail.com>
2019-05-05 14:07:01 +0900

    [ui] 퇴근가능시간 ui 구성

--- src/pages/WorkingTimePage/WorkingTimePage.tsx
+++ src/pages/WorkingTimePage/WorkingTimePage.tsx
@@ -1,6 +1,7 @@
 import {
   TextField,
   WithStyles,
+  Button,
 } from '@material-ui/core';
 import * as React from 'react';
 import styles from './WorkingTimePageStyled';
@@ -8,6 +9,7 @@ import styles from './WorkingTimePageStyled';
 interface IProps extends WithStyles<typeof styles> {
   
 }
+
 const WorkingTimePage: React.SFC<IProps> = () => (
   <>
     <h2>퇴근가능시간은?</h2>
@@ -18,13 +20,76 @@ const WorkingTimePage: React.SFC<IProps> = () => (
         // value={this.state.age}
         // onChange={this.handleChange('age')}
         type="number"
+        defaultValue="8"
         // className={classes.textField}
         InputLabelProps={{
           shrink: true,
         }}
         margin="normal"
       />
+      <TextField
+        id="standard-number"
+        label="Tuesday"
+        // value={this.state.age}
+        // onChange={this.handleChange('age')}
+        type="number"
+        defaultValue="8"
+        // className={classes.textField}
+        InputLabelProps={{
+          shrink: true,
+        }}
+        margin="normal"
+      />
+      <TextField
+        id="standard-number"
+        label="Wednesday"
+        // value={this.state.age}
+        // onChange={this.handleChange('age')}
+        type="number"
+        defaultValue="8"
+        // className={classes.textField}
+        InputLabelProps={{
+          shrink: true,
+        }}
+        margin="normal"
+      />
+      <TextField
+        id="standard-number"
+        label="Thursday"
+        // value={this.state.age}
+        // onChange={this.handleChange('age')}
+        type="number"
+        defaultValue="8"
+        // className={classes.textField}
+        InputLabelProps={{
+          shrink: true,
+        }}
+        inputProps={{
+          step: 0.1, // 5 min
+        }}
+        margin="normal"
+      />
+      <TextField
+        id="time"
+        label="Friday"
+        type="time"
+        defaultValue="08:00"
+        // className={classes.textField}
+        InputLabelProps={{
+          shrink: true,
+        }}
+        inputProps={{
+          step: 60, // 5 min
+        }}
+      />
     </div>
+    <Button
+      variant="contained"
+      color="primary"
+      // className={classes.button}
+    >
+      {'적용'}
+    </Button>
   </>
 )
 
--- src/pages/WorkingTimePage/WorkingTimePageStyled.ts
+++ src/pages/WorkingTimePage/WorkingTimePageStyled.ts
@@ -4,12 +4,18 @@ import {
 } from '@material-ui/core';
 
 const styles = (theme: Theme) => createStyles({
+  button: {
+    margin: theme.spacing.unit,
+  },
   container: {
     display: 'flex',
     flexWrap: 'wrap',
   },
   dense: {
     marginTop: 19,
+  },
+  input: {
+    display: 'none',
   },
   menu: {
     width: 200,
