32fd19d070c623ba369b5c0a53fa145c1dc3b172
Thor.Kim <kimdaesang@gmail.com>
2020-10-18 21:05:54 +0900

    테스트

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {
   Route,
   Switch,
 } from 'react-router-dom';
+import * as firebase from 'firebase';
 import Header from './components/Header/Header';
 import {
   FoodistPage,
@@ -13,8 +14,26 @@ import {
   WorkingTimePage,
 } from './pages';
 
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAacrWHJhs2AccoU-9GCEb-mmLRJsG0XcY",
+  authDomain: "office-htw.firebaseapp.com",
+  databaseURL: "https://office-htw.firebaseio.com",
+  projectId: "office-htw",
+  storageBucket: "office-htw.appspot.com",
+  messagingSenderId: "724085480815",
+  appId: "1:724085480815:web:4a78d1a04b7b5d0be45c9a",
+  measurementId: "G-TMMQR2HL44"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 class App extends React.Component {
+  static defaultProps = { database: {}};
   public render() {
+    const database = firebase.database();
+    console.log(database);
     return (
       <Router>
         <Header />
--- src/pages/FoodistPage/FoodistPage.tsx
+++ src/pages/FoodistPage/FoodistPage.tsx
@@ -3,6 +3,8 @@ import * as React from 'react';
 const FoodistPage = () => (
   <>
     <img style={{ width: '100%' }} src={process.env.PUBLIC_URL + "/foodist.png"} />
+    {/* <img style={{ width: '100%' }} src={'https://firebasestorage.googleapis.com/v0/b/my-console-19c02.appspot.com/o/foodist.png?alt=media&token=92963c70-2951-4338-9d15-5eb29b76a441'} /> */}
+    {/* <img style={{ width: '100%' }} src={'https://firebasestorage.googleapis.com/v0/b/my-console-19c02.appspot.com/o/foodist.png?alt=media&token=52f637f2-97cb-4526-abae-ea8a38c26dff'} /> */}
   </>
 )
 
