192821b2f0b90f3ee8bd3777e7ea2763778d14ca
Thor.Kim <kimdaesang@gmail.com>
2020-11-27 00:31:15 +0900

    [ImageUpload] 업로드 보완 처리
     - 파일 업로드를 무작위로 할 수 있어서 이슈가 생길수도 있다고 생각 함
     - 패스워드를 걸어둠
     - 패스워드는 그냥 일반 스트링 비교라..

--- src/pages/FoodistUploadPage/FoodistUploadPage.jsx
+++ src/pages/FoodistUploadPage/FoodistUploadPage.jsx
@@ -2,9 +2,25 @@ import React, { useEffect } from 'react';
 import { UploadStyled } from './FoodistUploadPageStyled';
 import { Input, Button } from '@material-ui/core';
 import * as firebase from 'firebase';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 const FoodistUploadPage = () => {
   const [imageSrc, setImageSrc] = React.useState('');
+  const [open, setOpen] = React.useState(false);
+  const [message, setMessage] = React.useState('');
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   React.useEffect(() => {
     const database = firebase.database();
@@ -26,18 +42,52 @@ const FoodistUploadPage = () => {
     }
   }
   const onSubmitClick = () => {
-    // 이미지 저장
-    const database = firebase.database();
-    database.ref('images/').update({
-      imageSrc,
-    });
+    const pw = document.getElementById('pw');
+    if (pw.value !== '4321') {
+      setMessage('deny');
+      handleClose();
+    } else {
+      // 이미지 저장
+      const database = firebase.database();
+      database.ref('images/').update({
+        imageSrc,
+      });
+      setMessage('okay');
+      handleClose();
+    }
   }
   return (
     <UploadStyled>
       <label>Upload</label>
       <Input type="file" onChange={onFileChange}>클릭</Input>
-      <Button onClick={onSubmitClick}>Submit</Button>
+      <Button onClick={handleClickOpen}>Submit</Button>
+      <div style={{ color: 'red' }}>{message}</div>
       <img style={{ width: '100%' }} src={imageSrc} />
+      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Warning</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To submit to this image, please enter your password code.
+          </DialogContentText>
+          <TextField
+            autoComplete="off"
+            autoFocus
+            margin="dense"
+            id="pw"
+            label="password"
+            type="password"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={onSubmitClick} color="primary">
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog>
     </UploadStyled>
   );
 }
