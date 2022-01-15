934f3e9d1f0a36c11da59cc4a5c75901a6e7b26e
Thor.Kim <kimdaesang@gmail.com>
2022-01-15 18:42:55 +0900

    fix: image 삭제
     - 더 이상 사진을 표시하지 않음

--- src/pages/Animals/AnimalsPage.jsx
+++ src/pages/Animals/AnimalsPage.jsx
@@ -3,14 +3,9 @@ import firebase from 'firebase/app';
 import {
   Button,
 } from '@material-ui/core';
-import i20210411_074214 from '../../res/images/20210411_074214.jpg';
-import i20210415_182520 from '../../res/images/20210415_182520.jpg';
-import i20210417_200729 from '../../res/images/20210417_200729.jpg';
-import i20210418_135413 from '../../res/images/20210418_135413.jpg';
-import i20210418_135759 from '../../res/images/20210418_135759.jpg';
-import i20210418_210314 from '../../res/images/20210418_210314.jpg';
 
-const newsUri = 'v1/search/news.json';
+
+// const newsUri = 'https://openapi.naver.com/v1/search/news.json';
 
 const AnimalsPage = () => {
   const [show, setShow] = useState(false);
@@ -66,7 +61,7 @@ const AnimalsPage = () => {
         'X-Naver-Client-Secret': 'IaPXGZ_j9D',
       },
     }
-    const data = getNewsAPI(newsUri, options);
+    // const data = getNewsAPI(newsUri, options);
   }
 
   const getNewsAPI = async (url, options) => {
@@ -120,12 +115,6 @@ const AnimalsPage = () => {
       </div>
       { show && (
         <div>
-          <img style={{ width: '100%' }} src={i20210411_074214} />
-          <img style={{ width: '100%' }} src={i20210415_182520} />
-          <img style={{ width: '100%' }} src={i20210417_200729} />
-          <img style={{ width: '100%' }} src={i20210418_135413} />
-          <img style={{ width: '100%' }} src={i20210418_135759} />
-          <img style={{ width: '100%' }} src={i20210418_210314} />
         </div>
       )}
     </div>
