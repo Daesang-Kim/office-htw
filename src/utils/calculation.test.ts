a964225a01c6abddefe2b88b0eb89ca82be23e07
thor.kim <kimdaesang@gmail.com>
2022-02-26 18:24:26 +0900

    fix: cypress test 문법 적용 한 부분 제거

--- src/utils/calculation.test.ts
+++ src/utils/calculation.test.ts
@@ -4,30 +4,30 @@ import {
 
 describe('Pad2 Function Testing', () => {
   it('string 2 => string 02', () => {
-    expect(pad2('2')).to.be('02');
+    expect(pad2('2')).toBe('02');
   });
   it('number 2 => string 02', () => {
-    expect(pad2(2)).to.equal('02');
+    expect(pad2(2)).toEqual('02');
   });
   it('string empty => string 0', () => {
-    expect(pad2('')).to.be('0');
+    expect(pad2('')).toBe('0');
   });
   it('string empty => string 0', () => {
-    expect(pad2('123')).to.be('123');
+    expect(pad2('123')).toBe('123');
   });
   it('0 => string 00', () => {
-    expect(pad2(0)).to.be('00');
+    expect(pad2(0)).toBe('00');
   });
   it('number 12 => string 12', () => {
-    expect(pad2(12)).to.be('12');
+    expect(pad2(12)).toBe('12');
   });
 })
 
 describe('toNumber Function Testing', () => {
   it('string 0 => number 0', () => {
-    expect(toNumber('0')).to.equal(0)
+    expect(toNumber('0')).toEqual(0)
   })
   it('string empty => number 0', () => {
-    expect(toNumber('')).to.equal(0)
+    expect(toNumber('')).toEqual(0)
   })
 })
